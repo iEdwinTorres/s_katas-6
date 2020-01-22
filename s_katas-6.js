ba3704c66c92bf051df5ce675d38f0e6bfe86909
iEdwinT <edwin_torres@icloud.com>
2020-01-22 01:21:47 -0500

    Finished assessmenet.

--- s_katas-6.js
+++ s_katas-6.js
@@ -1,26 +1,25 @@
 /* 
  * 1. Convert this to an arrow function
  */
-function add(x, y) {
+const add = (x, y) => {
     return x + y;
-}
+};
 
 /*
  * 2. If a function only has one argument, you can exclude the parentheses.
  *    Convert this to an arrow function that takes advantage of that.
  */
-function increment(number) {
+const increment = number => {
     return number + 1;
-}
+};
 
 /* 
  * 3. If a function's body includes a single expression, you can drop the curly
  *    braces and the return statement. Update this arrow function to be a one
  *    liner which doesn't include "return".
  */
-const decrement = number => {
-    return number - 1;
-};
+const decrement = number => number - 1;
+
 
 module.exports = {
     add,
